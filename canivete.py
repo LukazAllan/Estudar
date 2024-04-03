d3175fe9e8ed440ab514a22bd79b9b4b1cf81f19
Allan Lucas <38227482+LukazAllan@users.noreply.github.com>
2024-04-03 06:50:28 -0300

    Add files via upload
    Git init

---
+++ canivete.py
@@ -0,0 +1,154 @@
+from os import path, system
+
+
+def tit(txt):
+    t = int(len(txt) + 8)
+    print('-' * t)
+    print(f'{txt:^{t}}')
+    print('-' * t)
+
+
+def semacento(st):
+    #print("tô aq")
+    s = None
+    if type(st) == str:
+        s = list(st)
+    else:
+        s = st.copy()
+    for c in range(0,len(s)):
+        s[c] = s[c].lower()
+        if 'é' in s[c]:
+            #print('achei é')
+            s[c] = s[c].replace('é','e')
+        if 'ê' in s[c]:
+            #print('achei ê')
+            s[c] = s[c].replace('é','e')
+        if 'á' in s[c]:
+            #print('achei á')
+            s[c] = s[c].replace('á','a')
+        if 'ã' in s[c]:
+            #print('achei ã')
+            s[c] = s[c].replace('ã','a')
+        if 'â' in s[c]:
+            #print('achei é')
+            s[c] = s[c].replace('â','a')
+        if 'í' in s[c]:
+            #print('achei í')
+            s[c] = s[c].replace('í','i')
+        if 'ó' in s[c]:
+            #print('achei ó')
+            s[c] = s[c].replace('ó','o')
+        if 'ô' in s[c]:
+            #print('achei ô')
+            s[c] = s[c].replace('ô','o')
+        if 'õ' in s[c]:
+            #print('achei ó')
+            s[c] = s[c].replace('õ','o')
+        if 'ú' in s[c]:
+            #print('achei ú')
+            s[c] = s[c].replace('ú','u')
+        if 'ç' in s[c]:
+            #print('achei ç')
+            s[c] = s[c].replace('ç','c')
+    if type(st) == str:
+        return ''.join(s)
+    else:
+        return s
+
+
+def cor(md=None):
+    dic={
+    	None:'\033[m',
+    	'química':'\033[1;35;43m',
+    	'biologia':'\033[1;32;43m',
+    	'física':'\033[1;36;43m',
+    	
+    	'matemática':'\033[1;33;44m',
+    	
+    	'história':'\033[1;34;44m',
+    	'geografia' :'\033[1;34;44m',
+    	'filosofia':'\033[1;34;44m',
+    	'sociologia':'\033[1;34;44m',
+    	
+    	'gramática':'\033[1;36;41m',
+    	'literatura':'\033[1;36;41m'
+    }
+    try:
+        return dic[md]
+    except:
+        return ''
+
+
+def cls():
+    android: bool = path.exists('/storage/emulated/0')
+    windows: bool = path.exists('C:/Program Files')
+    linux: bool = path.exists('/home/')
+    if android or linux:
+        system('clear')
+    if windows:
+        system('cls')
+
+
+def limpa():
+    """
+    Função que limpa a tela
+    """
+    if path.isdir('C:/'):
+        system('cls')
+    if path.isdir('/storage/emulated/0/'):
+        system('clear')
+
+
+# noinspection PyArgumentList
+def linque(caminho, form='r', asci=True):
+    """
+    Função que linca o arquivo
+    :type asci: bool
+    :type form: object
+    :param caminho: indica o caminho do arquivo
+    :param asci: leitura de códigos ascii, que
+    para ativar escreva False para ele
+    :return: a
+    """
+    if path.isfile(caminho, form):
+        return open(caminho, form, ensure_ascii=asci)
+
+
+def pc():
+    if path.isdir('C:/'):
+        return 'windows'
+    elif path.isdir('/storage/emulated/0/'):
+        return 'android/linux'
+
+
+def rinput(texto: object, b: object = None, enfeite: object = ':', errormsg = 'Tente novamente.'):
+    """
+    Função que não para de executar input
+    até que você digite alguma coisa ou
+    digite a coisa correta.
+    :type texto: object
+    :param texto: Exibe no input
+    :type b: object
+    :param b: lista de condição
+    :return: var c
+    """
+    if b is None:
+        b = False
+    cont = -1
+    while True:
+        cont += 1
+        if cont == 0:
+            c = input(f'{texto}{enfeite} ')
+        else:
+            c = input(f'{errormsg}\n{texto}{enfeite} ')
+        if c == '':
+            limpa()
+            pass
+        else:
+            if not b:
+                return str(c)
+            else:
+                if c in b:
+                    return str(c)
+                else:
+                    pass
---
+++ estudar0.py
@@ -0,0 +1,97 @@
+# Python 3.12
+"""
+param log: logging module,
+param data: load file
+param d:
+param j:
+param r:
+param r2:
+param q:
+Δθλ¹²³⁰⁴⁵⁶⁷⁸⁹₀₁₂₃₄₅₆₇₈₉⁺⁻⁽⁾"""
+from logging import Logger
+import canivete
+import json
+import logging
+import os
+import os.path as path
+from random import randint
+from canivete import limpa, cor
+"""class ArquivoJsonErro(Exception):
+    def __init__(self,msg):
+        self.msg(msg)"""
+
+arquivo = 'estudar0'
+logging.basicConfig(filename=f'{arquivo}.py.log', filemode='w', encoding='utf-8', format='%(asctime)s %(levelname)s - %(message)s',
+                    level=logging.DEBUG)
+log: Logger = logging.getLogger()
+log.debug('__init__')
+
+if not path.isfile(f'{arquivo}.json'):
+    log.critical('O arquivo não foi carregado')
+    print('O arquivo não foi carregado')
+    pass
+else:
+    with open(f'{arquivo}.json', 'r', encoding='utf-8') as file:
+        data = dict(json.load(file))
+        # except:
+        # raise ArquivoJsonErro
+        file.close()
+    log.debug('loading...')
+    j = {'p': 0, 'r': 0}
+    while True:
+        limpa()
+        log.debug(f"rodada {j['r']}")
+        j['r'] += 1
+        caminho = data
+        ra = ''
+        for c in range(10):
+            #input(caminho)
+            if type(caminho) == dict:
+                escolha = randint(0, len(list(caminho.keys())) - 1)
+                if c == 0:
+                    rm = list(caminho.keys())[escolha].lower()
+                if c > 0:
+                    ra = ra + f'{list(caminho.keys())[escolha]}'
+                caminho = caminho[list(caminho.keys())[escolha]]
+            else:
+                d: object = caminho[randint(0, len(caminho) - 1)].copy()
+                break
+
+        # rm = list(data.keys())[randint(0, len(list(data.keys()))-1)]
+        # log.info(f'rm = {rm}')
+        # ra = list(data[rm].keys())[randint(0, len(list(data[rm].keys()))-1)]
+        # log.info(f'ra = {ra}')
+        # d = data[rm][ra]
+        # input(d)
+        q = None
+        print(f'rod: {j["r"]}  pto: {j["p"]}\nsobre {rm} em {ra}:')
+        log.info(f'0={d[0]} 1={d[1]}, ')
+        sl = randint(0, len(d[1]) - 1)
+        if type(d[1]) == list:
+            log.info(f'd[1][sl] = {d[1][sl]}')
+            while not q or q == '':
+                q = input(f'{cor(rm)}{d[1][sl]}{cor()} ')
+            log.info(f'STDIN: input\nrod: {j["r"]}  pto: {j["p"]}\nsobre {rm} em {ra}:\n{d[1][sl]}')
+        elif type(d[1]) == str:
+            while not q or q == '':
+                q = input(f'{cor(rm)}{d[1]}{cor()} ')
+            log.info(f'STDIN: input\nrod: {j["r"]}  pto: {j["p"]}\nsobre {rm} em {ra}:\n{d[1]}')
+
+        print(canivete.semacento(q), canivete.semacento(d[0]))
+        if type(d[0]) == str and canivete.semacento(q) == canivete.semacento(d[0]):
+            print('Certa resposta')
+            j['p'] += 1
+            log.info('str and certo')
+            log.info(f'True \'{q}\' == \'{d[0]}\'')
+        elif type(d[0]) == list and canivete.semacento(q) in canivete.semacento(d[0]):
+            print('Certa resposta')
+            j['p'] += 1
+            log.info('list and certo')
+            log.info(f'True \'{q}\' == \'{d[0]}\'')
+        else:
+            print(f'Incorreto.\nA resposta era {d[0]}')
+            log.info('errado')
+            log.info(f'False \'{q}\' != \'{d[0]}\'')
+        w = input('sair? ')
+        if not w == '':
+            break
